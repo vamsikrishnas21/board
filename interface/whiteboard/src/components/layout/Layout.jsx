cf078d78cc843d2d7863003606dff17c2d07a2a2
vamsikrishnas21 <35953141+vamsikrishnas21@users.noreply.github.com>
2022-08-11 03:45:22 +1300

    Add files via upload

--- interface/whiteboard/src/components/layout/Layout.jsx
+++ interface/whiteboard/src/components/layout/Layout.jsx
@@ -38,31 +38,11 @@ class Layout extends React.Component{
 
 
           
-               <h3 className="name-midile">Explain Everything Online Whiteboard</h3>
+               <h3 className="name-midile"></h3>
                 <div class="tools-section">
-                    <div className="color-picker-container">
-                        Select Brush Color : &nbsp;
-                        <input type="color" value={this.state.color} onChange={this.changeColor.bind(this)} />
-                    </div>
+                   
 <br />
-                    <div className="brushsize-container">
-                        Select Brush Size : &nbsp;
-                        <select value={this.state.size} onChange={this.changeSize.bind(this)}>
-                           <option>1</option>
-                           <option>5</option>
-                           <option>10</option>
-                           <option>15</option>
-                           <option>20</option>
-                           <option>25</option>
-                           <option>30</option>
-                           <option>35</option>
-                           <option>40</option>
-                           <option>45</option>
-                           <option>50</option>
-                           <option>55</option>
-
-                        </select>
-                    </div>
+                    
 
 
 
