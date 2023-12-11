2c556482fd043f9f9c404e11d92f63be2ce16768
vamsikrishnas21 <35953141+vamsikrishnas21@users.noreply.github.com>
2023-12-11 13:13:18 +0000

    Layout.jsx

--- interface/whiteboard/src/components/layout/Layout.jsx
+++ interface/whiteboard/src/components/layout/Layout.jsx
@@ -2,72 +2,53 @@ import React from 'react';
 import Whiteboard from '../whiteboard/Whiteboard';
 import './style.css';
 
-class Layout extends React.Component{
-
-    constructor(props){
+class Layout extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
             color: "#000000",
             size: "5",
-            username:"",
-            users:[{}]
-        }
-       
+        };
+        this.changeColor = this.changeColor.bind(this);
+        this.changeSize = this.changeSize.bind(this);
     }
     
-    changeColor(params) {
-        this.setState({
-            color: params.target.value
-        })
+    changeColor(event) {
+        this.setState({ color: event.target.value });
     }
 
-    changeSize(params) {
-        this.setState({
-            size: params.target.value
-        })
+    changeSize(event) {
+        this.setState({ size: event.target.value });
+    }
+
+    renderBrushSizeOptions() {
+        return [1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55].map(size => (
+            <option key={size} value={size}>{size}</option>
+        ));
     }
 
     render() {
-
         return (
-
-       
-            <div className="layout">         
-               <h3 className="name-midile"></h3>
-               <center><h2>Whiteboard</h2></center>
-                <div class="tools-section">
-              
-
+            <div className="layout">
+                <center><h2>Whiteboard</h2></center>
+                <div className="tools-section">
                     <div className="brushsize-container">
                         Brush Size : &nbsp;
-                        <select value={this.state.size} onChange={this.changeSize.bind(this)}>
-                           <option>1</option>
-                           <option>5</option>
-                           <option>10</option>
-                           <option>15</option>
-                           <option>20</option>
-                           <option>25</option>
-                           <option>30</option>
-                           <option>35</option>
-                           <option>40</option>
-                           <option>45</option>
-                           <option>50</option>
-                           <option>55</option>
-
+                        <select value={this.state.size} onChange={this.changeSize}>
+                            {this.renderBrushSizeOptions()}
                         </select>
                     </div>
-                    
+                    <div className="color-picker-container">
+                        Brush Color : &nbsp;
+                        <input type="color" value={this.state.color} onChange={this.changeColor} />
+                    </div>
                 </div>
-            
-                <div class="whiteboard-layout">
-                    
-                    <Whiteboard color={this.state.color} size={this.state.size}></Whiteboard>
+                <div className="whiteboard-layout">
+                    <Whiteboard color={this.state.color} size={this.state.size} />
                 </div>
-
             </div>
-
-        )
+        );
     }
 }
 
-export default Layout
+export default Layout;
